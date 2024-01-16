9733dd10e919cdf23adc5be66ba28d88826a218f
Luka Wanyama <lukewanyama5@gmail.com>
2024-01-16 22:47:13 +0300

    Adjustments

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
+
 document.addEventListener('DOMContentLoaded', function () {
+    const movieList = document.querySelector('ul');
+    let availableSlots = 0; 
 
-fetch("http://localhost:3000/films")
-.then(response => response.json())
-.then(data => {
-    data.forEach(film => {
-        const inputData = `
-        <img src="${film.poster}" alt="${film.title} Poster">
-       <p id="titleCase"><b>${film.title}</b></p>
-        <p><li>Runtime: ${film.runtime} minutes </li></p>
-        <p><li>Showtime: ${film.showtime}</li></p>
-        <p><li>Description: ${film.description}</li></p>
-        <p><li>Tickets Sold / Theatre Capacity: ${film.tickets_sold} / ${film.capacity}</li></p>
-        <button onclick="ticketSales(${film.tickets_sold}, ${film.capacity}, ${film.title})">Buy Ticket</button>
-        </li>
-`;
+    fetch("http://localhost:3000/films")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(film => {
+                const inputData = `
+                    <li id="${film.title}">
+                        <img src="${film.poster}" alt="${film.title} Poster">
+                        <p id="titleCase"><b>${film.title}</b></p>
+                        <ul>
+                            <li>Runtime: ${film.runtime} minutes </li>
+                            <li>Showtime: ${film.showtime}</li>
+                            <li>Description: ${film.description}</li>
+                            <li>Tickets Sold / Theatre Capacity: ${film.tickets_sold} / ${film.capacity}</li>
+                            <li><button>Buy Ticket</button>
+                        </ul>
+                    </li>
+                `;
+                movieList.insertAdjacentHTML('beforeend', inputData);
 
-document.querySelector('ul').insertAdjacentHTML('beforeend', inputData)
+                if (availableSlots === 0) {
+                    availableSlots = film.capacity;
+                }
+            });
 
-    })
-})
-.catch(error => console.error('error fetching data:', error))
+        
+            const buyTicketButton = document.createElement('button');
+            buyTicketButton.textContent = 'Buy Ticket';
+            buyTicketButton.addEventListener('click', function () {
+                
+                buyTicket();
+            });
+            document.body.appendChild(buyTicketButton);
+        })
+        .catch(error => console.error('error fetching data:', error));
 
-})
-
-
-function ticketSales(tickets_sold, capacity, title) {
-    const remainingTickets = capacity - tickets_sold;
-    if (tickets_sold === capacity) {
-        console.log('The theatre is at capacity for ' + title);
-    } else if (tickets_sold < capacity) {
-        console.log('Remaining tickets for ' + title + ': ' + remainingTickets);
-    } else {
-        tickets_sold++;
-        console.log('Ticket sold for ' + title + 'Tickets ' + tickets_sold);
-
-        const ticketsInfoElement = document.querySelector(`#${title} > p > li:nth-child(5)`);
-        ticketsInfoElement.textContent = `Tickets Sold / Theatre Capacity: ${tickets_sold} / ${capacity}`;
+    function buyTicket() {
+        if (availableSlots > 0) {
+            availableSlots--;
+            console.log('Ticket bought! Available slots: ' + availableSlots);
+        } else {
+            console.log('The theatre is at full capacity.');
+        }
     }
-}
+});
+
+
